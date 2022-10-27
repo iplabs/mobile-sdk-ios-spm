2eeb8960551d5fea9634a45fe220720bf66f3a7b
Raphael Schroiff <r.schroiff@iplabs.de>
2022-10-27 17:39:23 +0200

    Release v1.0.0

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
                   name: "IplabsSdk",
-                  url: "https://mobile-sdk.s3.eu-central-1.amazonaws.com/iOS/IplabsSdk.xcframework-v0.2.zip",
-                  checksum: "ecfa61de1aa6187fdac49f9c929174105e80b0822180f5d0d24dd439f008b080"
+                  url: "https://mobile-sdk.s3.eu-central-1.amazonaws.com/iOS/IplabsSdk.xcframework-v1.0.0.zip",
+                  checksum: "2ba228702da80cdd9e55e9ddcac8395a9e2e0f0b7fc1a4b3cc8b8958892f17f9"
               ),
     ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ip.labs Mobile SDK Swift package
 
 Swift package for the ip.labs Mobile SDK
+
+Documentation can be found at <http://mobile-sdk-docs.s3-website.eu-central-1.amazonaws.com>
